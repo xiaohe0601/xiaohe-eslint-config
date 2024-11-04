f189d6fdda5d6d8491effd0b6ba73af190492353
xiaohe0601 <xiaohe0601@outlook.com>
2024-11-04 13:15:01 +0800

    chore: fix quotes type error

--- src/types.ts
+++ src/types.ts
@@ -191,7 +191,8 @@ export interface OptionsStylistic {
 }
 
 export interface StylisticConfig
-  extends Pick<StylisticCustomizeOptions, "indent" | "quotes" | "jsx" | "semi"> {
+  extends Pick<StylisticCustomizeOptions, "indent" | "jsx" | "semi"> {
+  quotes?: "single" | "double";
 }
 
 export interface OptionsOverrides {
