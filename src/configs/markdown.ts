76e806666987ac42fc67941e825d2effd37532f4
xiaohe0601 <xiaohe0601@outlook.com>
2025-06-10 19:22:31 +0800

    fix: cleanup disable usages

--- src/configs/markdown.ts
+++ src/configs/markdown.ts
@@ -56,8 +56,6 @@ export async function markdown(
       rules: {
         "antfu/no-top-level-await": "off",
 
-        "import/newline-after-import": "off",
-
         "no-alert": "off",
         "no-console": "off",
         "no-labels": "off",
@@ -66,11 +64,13 @@ export async function markdown(
         "no-undef": "off",
         "no-unused-expressions": "off",
         "no-unused-labels": "off",
-
         "no-unused-vars": "off",
+
         "node/prefer-global/process": "off",
+
         "style/comma-dangle": "off",
         "style/eol-last": ["error", "never"],
+        "style/padding-line-between-statements": "off",
 
         "ts/consistent-type-imports": "off",
         "ts/explicit-function-return-type": "off",
